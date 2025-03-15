1048b83f703927517ee8324467db96bc0bf8c50c
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-15 17:36:00 +0200

    two days date

--- greeting.py
+++ greeting.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 print("HELLO WORLD")
 greeting_version = 3.0
@@ -9,3 +9,4 @@ today = date.today()
 
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
+two_days = today + datetime.timedelta(days=2)
