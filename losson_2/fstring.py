9aaad506a017d1a3f350180e92a85677a93caec9
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-16 16:04:09 +0200

    types of data + fstring

--- losson_2/fstring.py
+++ losson_2/fstring.py
@@ -1,5 +1,19 @@
-sofa_name = "Sofia"
-sofa_age = 18
-studied_years = 4
+import datetime
 
-print(f"My name is {sofa_name} and my age is {sofa_age}. I am studying for {studied_years + 1} years\n")
+sofa_name: str = "Sofia"
+sofa_age: str = 18
+studied_years: int = 4
+
+f_string = f"My name is {sofa_name} and my age is {sofa_age}. I am studying for {studied_years + 1} years\n"
+
+sofa_age = 22
+
+print(f_string)
+
+
+
+your_name: str = input("What is your name? ")
+your_age: int = input("What is your age? ")
+
+f_string = f"Your data: Name - {your_name}, Age - {your_age}\nYou was born in {datetime.datetime.now().year - your_age}"
+print(f_string)
---
+++ losson_2/types_of_data.py
@@ -0,0 +1,12 @@
+# Незмінні (Immutable)
+int_var = 42                  # int
+float_var = 3.14              # float
+bool_var = True               # bool
+str_var = "Hello, Python!"    # str
+tuple_var = (1, 2, 3)         # tuple
+frozenset_var = frozenset({1, 2, 3})  # frozenset
+
+# Змінні (Mutable)
+list_var = [1, 2, 3]          # list
+set_var = {1, 2, 3}           # set
+dict_var = {"key": "value"}   # dict
