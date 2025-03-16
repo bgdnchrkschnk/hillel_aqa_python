6073b4abdd200d82f26f361f4bf4ae23ee870fd8
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-16 13:28:41 +0200

    strings added

---
+++ losson_2/strings.py
@@ -0,0 +1,11 @@
+s_1 = 'the world is mine'
+
+s_2 = "the world is mine"\
+    "How is your mood today?"\
+    ""
+s_3 = """
+the world is mine
+HELLO, man!How is your mood today?"""
+print(s_1)
+print(s_2)
+print(s_3)
