b3ce75ac66126bb3e79d83f42293a8929cd09b45
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-16 13:58:23 +0200

    escaping

--- losson_2/strings.py
+++ losson_2/strings.py
@@ -1,11 +1,12 @@
 s_1 = 'the world is mine'
 
 s_2 = "the world is mine"\
-    "How is your mood today?"\
-    ""
+    "How is your mood today?"
+
 s_3 = """
 the world is mine
 How is your mood today?"""
+
 print(s_1)
 print(s_2)
 print(s_3)
