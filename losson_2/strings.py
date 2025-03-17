24694b6f96b6769dd49e9ec689d2f6dd1f5e7992
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-17 19:19:12 +0200

    t.py gitignore

--- losson_2/strings.py
+++ losson_2/strings.py
@@ -1,15 +1,17 @@
-s_1 = 'the world is mine'
+# s_1 = 'the world is mine'
+#
+# s_2 = "the world is mine" \
+#       "How is your mood today?"
 
-s_2 = "the world is mine" \
-      "How is your mood today?"
+whos = 'mine'
 
-s_3 = """
-the world is mine
+s_3 = f"""
+the world is {whos}
 How is your mood today?"""
-
-print(s_1)
-print(s_2)
-print(s_3)
-
-string_ = "I can't do it today"
-string_two = 'I can\'t do it today'
+#
+# print(s_1)
+# print(s_2)
+# print(s_3)
+#
+# string_ = "I can't do it today"
+# string_two = 'I can\'t do it today'
