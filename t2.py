ecd3b285606255aba41da9e3abb5735594406360
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-24 22:09:29 +0200

    gitignore

--- t2.py
+++ t2.py
@@ -19,58 +19,10 @@ hour after hour. And when the middle of the afternoon came, from being a
 poor poverty, stricken boy in the .... morning, Tom was literally
 rolling in wealth."""
 
-##  ПЕРЕЗАПИСУЙТЕ зміст змінної adwentures_of_tom_sawer у завданнях 1-3
-# task 01 ==
-""" Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
-треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
-
-adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
-print(adwentures_of_tom_sawer)
-
-# task 02 ==
-""" Замініть .... на пробіл
+# task 09
+""" Перевірте чи починається якесь речення з "By the time".
 """
-
-adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
-print(adwentures_of_tom_sawer)
-
-# task 03 ==
-""" Зробіть так, щоб у тексті було не більше одного пробілу між словами.
-"""
-
-adwentures_of_tom_sawer = " ".join(adwentures_of_tom_sawer.split())
-print(adwentures_of_tom_sawer)
-
-# task 04
-""" Виведіть, скількі разів у тексті зустрічається літера "h"
-"""
-number_of_letters = adwentures_of_tom_sawer.count("h")
-print(number_of_letters)
-
-# task 05
-""" Виведіть, скільки слів у тексті починається з Великої літери?
-"""
-capitalized_words_count = adwentures_of_tom_sawer.split()
-count_words = 0
-for word in adwentures_of_tom_sawer.split():
-    if word[0].istitle():
-         count_words += 1
-print(count_words)
-
-# task 06
-""" Виведіть позицію, на якій слово Tom зустрічається вдруге
-"""
-
-search_word = "Tom"
-search_index_1 = adwentures_of_tom_sawer.find(search_word)
-search_index_2 = adwentures_of_tom_sawer.find(search_word, search_index_1 + 1)
-print(search_index_2)
-
-# task 07
-""" Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
-Збережіть результат у змінній adwentures_of_tom_sawer_sentences
-"""
-
-adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split("\n")
-
-print(adwentures_of_tom_sawer_sentences)
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split(".")
+for some_sentences in adwentures_of_tom_sawer_sentences:
+    if some_sentences.startswith("\nBy the time"):
+        print("В тексті є речення яке починається з 'By the time'")
