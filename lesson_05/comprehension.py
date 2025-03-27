9e3dc848e6d0da7a76358db906c2e9adb996809e
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-03-27 19:06:53 +0200

    6 lesson

--- lesson_05/comprehension.py
+++ lesson_05/comprehension.py
@@ -8,5 +8,21 @@ print(range_obj)
 for obj in range_obj:
     print(obj)
 
+
 dict_comprehension: dict = {index: index**2 for index in range(11)} # {key: value }
 print(dict_comprehension)
+# --------------------------------- examples of dict comprehension
+# Test data generating
+user_ids = [101, 102, 103]
+test_users = {uid: f"user_{uid}" for uid in user_ids}
+# {101: 'user_101', 102: 'user_102', 103: 'user_103'}
+
+statuses = {
+    "payment1": "success",
+    "payment2": "failed",
+    "payment3": "success",
+    "payment4": "pending"
+}
+successful = {k: v for k, v in statuses.items() if v == "success"}
+# {'payment1': 'success', 'payment3': 'success'}
+
---
+++ t.py
@@ -0,0 +1,38 @@
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+people_records.insert(0, ('Lisa', 'Clark', 30, 'QA', 'Lviv'))
+
+people_records[1], people_records[5] = people_records[5], people_records[1]
+
+selected_indexes = [6, 10, 13]
+
+age_check_6 = people_records[6][2] >= 30
+age_check_10 = people_records[10][2] >= 30
+age_check_13 = people_records[13][2] >= 30
+
+is_over_30: bool = all([people_records[i][2] >= 30 for i in selected_indexes])
+print(is_over_30)
+
+print(f"person with index 6: {age_check_6}, person with index 10: {age_check_10}, person with index 13: {age_check_13}")
