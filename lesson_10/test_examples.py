a74cf3f57ea5ef3606d39c6f1c74f4e94030a871
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2025-04-14 21:37:03 +0300

    pytest

---
+++ lesson_10/test_examples.py
@@ -0,0 +1,75 @@
+import os
+
+import pytest
+from assertpy import assert_that
+env = os.getenv("ENV")
+
+def divide(x, y):
+    return x / y
+
+@pytest.mark.smoke
+class TestExamples:
+
+    BASE_URL = "http://example.com"
+
+    def setup_class(self):
+        print("precondition setup")
+
+    def test_sum_positive(self):
+        print("HELLO I AM PRINT")
+        assert 1 + 2 == 3
+        # assert_that(1+1).is_equal_to(3)
+
+    # @pytest.mark.parametrize("a, b, expected", [
+    #     (1, 2, 3),
+    #     (5, 5, 10),
+    #     (10, -2, 8)
+    # ])
+    # def test_addition(self, a, b, expected):
+    #     assert a + b == expected
+    #
+    #
+    def test_zero_division_negative(self):
+        with pytest.raises(ZeroDivisionError):
+            divide(10, 0)
+
+    @pytest.mark.xfail
+    def test_multiply(self):
+        assert 3 * 3 == 10
+
+    @pytest.mark.skip(reason="need to bugfix BOF-75")
+    def test_subtract(self):
+        assert 5 - 2 == 3
+    #
+    # @pytest.fixture
+    # def sample_list(self):
+    #     return [1, 2, 3]
+    #
+    # def test_list_length(self, sample_list):
+    #     assert len(sample_list) == 3
+    #
+    # def test_list_sum(self, sample_list):
+    #     assert sum(sample_list) == 6
+    #
+    # @pytest.mark.skip(reason="Функціонал ще не реалізований")
+    # def test_skipped(self):
+    #     assert 1 == 1
+
+def define_env(env):
+    return True if env == "prod" else False
+
+    import sys
+
+    @pytest.mark.skipif(env == "prod", reason="Не підтримується на Windows")
+    def test_not_on_windows(self):
+        assert True
+    #
+    # @pytest.mark.xfail(reason="Відомий баг #123")
+    # def test_known_bug(self):
+    #     assert 2 + 2 == 5
+
+    # @pytest.mark.parametrize("status", ["ok", "error", "unknown"])
+    # def test_status_handler(self, status):
+    #     if status not in ("ok", "error"):
+    #        pytest.fail(f"Невідомий статус: {status}")
+
--- t.py
+++ t.py
@@ -1,198 +1,26 @@
-# task 1
-""" Задача - надрукувати табличку множення на задане число, але
-лише до максимального значення для добутку - 25.
-Код майже готовий, треба знайти помилки та випраавити\доповнити.
-"""
-from os import strerror
+# Завдання - Створіть масив зі строками, які будуть складатися з чисел, які розділені комою. Наприклад:
+# [”1,2,3,4”, ”1,2,3,4,50” ”qwerty1,2,3”]
+# Для кожного елементу списку виведіть суму всіх чисел (створіть нову функцію для цього).
+# Якщо є символи, що не є числами (”qwerty1,2,3” у прикладі), вам потрібно зловити вийняток і вивести “Не можу це зробити!”
+# Використовуйте блок try\except, щоб уникнути інших символів, окрім чисел у списку.
+# Для цього прикладу правильний вивід буде - 10, 60, “Не можу це зробити”
 
+def sum_numbers(s: str) -> int | str:
+    try:
+        numbers = s.split(',')  # Розбиваю рядок на частини по комі
+        total = 0
 
-def multiplication_table(number, maxResult):
-    # Initialize the appropriate variable
-    multiplier = 1
+        for num in numbers:
+            total += int(num)
+        return total
 
-    # Complete the while loop condition.
-    while multiplier * number <= maxResult:
-        result = number * multiplier
-        # десь тут помила, а може не одна
-        if  result >= maxResult:
-            # Enter the action to take if the result is greater than 25
-            pass
-        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+    except:
+        return "Не можу це зробити!"  # Якщо буде помилка — повертаю повідомлення
 
-        # Increment the appropriate variable
-        multiplier += 1
+# Створив список рядків, де деякі містять лише числа, а деякі — ще й текст
+array = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
 
-multiplication_table(3, 25)
-# Should print:
-# 3x1=3
-# 3x2=6
-# 3x3=9
-# 3x4=12
-# 3x5=15
-
-
-# task 2
-"""  Написати функцію, яка обчислює суму двох чисел.
-"""
-def make_summ(x, y):
-    print(x + y)
-
-make_summ(2,3)
-
-# task 3
-"""  Написати функцію, яка розрахує середнє арифметичне списку чисел.
-"""
-def everage_num (*some_list):
-    result_everage_num = 0
-    for i in some_list:
-        result_everage_num +=i
-    print(result_everage_num / len(some_list))
-
-everage_num(1,2,3,4,5)
-
-# task 4
-"""  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
-"""
-def oposit_str(your_str):
-    print(your_str[::-1])
-
-oposit_str("qwerty12345")
-# task 5
-"""  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
-"""
-def the_longest(some_list):
-    len_of_list = [len(i) for i in some_list]
-    indx_of_max_len = len_of_list.index(max(len_of_list))
-    print(some_list[indx_of_max_len])
-
-the_longest(['KwFaLbQ', 'xVjP', 'RstyZm', 'AhDgEk', 'plQrS111111'])
-
-# task 6
-"""  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
-у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
-не є підрядком першого рядка."""
-def find_substring(str1, str2):
-    if str2 in str1:
-        index = str1.index(str2[0])
-        return index
-    else:
-        return -1
-
-
-str1 = "Hello, world!"
-str2 = "world"
-print(find_substring(str1, str2)) # поверне 7
-
-str1 = "The quick brown fox jumps over the lazy dog"
-str2 = "cat"
-print(find_substring(str1, str2)) # поверне -1
-
-# task 7
-# task 8
-# task 9
-# task 10
-"""  Оберіть будь-які 4 таски з попередніх домашніх робіт та
-перетворіть їх у 4 функції, що отримують значення та повертають результат.
-Обоязково документуйте функції та дайте зрозумілі імена змінним.
-"""
-#Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
-random_list = [42, 87, 23, 91, 56, 14, 68, 79, 35, 10, 99, 74, 62, 5, 31]
-
-sum = 0
-
-for i in random_list:
-    if i % 2 == 0:
-        sum += i
-
-print(sum)
-
-
-#Є list з даними . Напишіть код, який свормує новий list (наприклад lst2),
-# який містить лише змінні типу стрінг, які присутні в lst1.
-# Данні в лісті можуть бути будь якими
-
-def list_with_str(lst):
-    list_str_result = []
-    for i in lst:
-        if type(i) == str:
-            list_str_result.append(i)
-    print(list_str_result)
-
-
-list_with_str(['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'])
-
-#Напишіть цикл, який буде вимагати від користувача ввести слово,
-# в якому є літера "h" (враховуються як великі так і маленькі).
-# Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
-
-def give_me_h(my_str):
-    lower_my_str = my_str.lower()
-    while "h" not in lower_my_str:
-        one_more_try = input("Введи ще раз:")
-        return give_me_h(one_more_try)
-    return print("дякую")
-
-
-
-        #my_str = input("Введи слово, в якому буде хоч одна 'h' :")
-        # find_h = my_str.lower()
-        # if find_h.find("h") != -1:
-        #     print("дякую")
-        #     return True
-        # else:
-        #     one_more_try = input("Введи ще раз:")
-        #     give_me_h(one_more_try)
-
-#Порахувати кількість унікальних символів в строці.
-# Якщо їх більше 10 - вивести в консоль True, інакше - False.
-# Строку отримати за допомогою функції input()
-
-def counting_uniqe(some_input):
-    if len(set(some_input)) > 10:
-        print(True)
-    else:
-        print(False)
-
-
-import unittest
-
-
-class maker_sum_test(unittest.TestCase):
-    """
-    Тестуємо функцію make_summ
-    """
-    def test_if_equal(self):
-        self.assertEqual(make_summ(2,3), 5)
-    def test_if_not_equal(self):
-        self.assertNotEqual(make_summ(2,3), 6)
-    def test_if_type_error(self):
-        self.assertRaises(TypeError, make_summ, "2", 3)
-    def test_with_float_only(self):
-        self.assertEqual(make_summ(2.5, 3.5), 6)
-    def test_with_float_second(self):
-        self.assertEqual(make_summ(2.05, 3.04), 5.09)
-
-class average_num_test(unittest.TestCase):
-    """
-    Тестуємо функцію average_num - середнє значення
-    """
-    def test_if_equal(self):
-        self.assertEqual(average_num(2,3), 2.5)
-    def test_with_float(self):
-        self.assertEqual(average_num(2.5, 3.5, 4, 4.05), 3.5125)
-    def test_if_type_error(self):
-        self.assertRaises(TypeError, average_num, "2", 3)
-class list_with_str_test(unittest.TestCase):
-    """
-    Тестуємо функцію яка фільтрує список і повертає значення з типом str
-    """
-    def test_if_equal(self):
-        self.assertEqual(list_with_str([1, "2", "3"]), ["2", "3"])
-        self.assertEqual(list_with_str([True, "2", "3"]), ["2", "3"])
-        self.assertEqual(list_with_str([None, "2", "3"]), ["2", "3"])
-        self.assertEqual(list_with_str(["", "2", "3"]), ["", "2", "3"])
-
-class counting_unique_test(unittest.TestCase):
-    def test_if_equal(self):
-        self.assertEqual(counting_unique([1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11]), True)
-        self.assertEqual(counting_unique(["h", "h", "h", "h", "h", "h", "h"]), False)
+# Пройшовся по кожному елементу масиву і викликав функцію
+for item in array:
+    result = sum_numbers(item)
+    print(result)  # Вивожу результат (або суму, або повідомлення про помилку)
